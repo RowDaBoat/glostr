d4d1cfc6fe78fede78b8cf764c47d56f4f319694
Row <reardenr@gmail.com>
2025-01-06 02:13:13 -0300

    Updates

--- shaders/cyber-fuji.glsl
+++ shaders/cyber-fuji.glsl
@@ -1,7 +1,9 @@
 #version 300 es
 precision mediump float;
 
-//Original shader: Cyber Fuji by kaiware007: https://www.shadertoy.com/view/Wt33Wf
+// Original shader:
+// Cyber Fuji by kaiware007
+// https://www.shadertoy.com/view/Wt33Wf
 
 uniform vec2 u_resolution;
 uniform float u_time;
